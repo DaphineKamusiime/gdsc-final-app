e0a65d06453fee4056cea7161c10ad23fa8eabff
Daphine Kamusiime <daphnekamusiime09@gmail.com>
2023-04-15 15:40:14 +0300

    add logout

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,9 +1,19 @@
 import { Link, useNavigate } from "react-router-dom";
 import "./navigationBar.css";
 import Logo from "../Logo/logo";
+import { useUserAuth } from "../../utils/UserAuthContext";
 
 function NavigationBar() {
   const navigate = useNavigate();
+  const { user, logOut } = useUserAuth();
+  const handleLogOut = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="navbar-container">
       <div>
@@ -24,18 +34,41 @@ function NavigationBar() {
       </div>
       <div className="navbtns-container">
         {/* <buttons  */}
-        <button
-          style={{ margin: "0 20px", backgroundColor: "blue", color: "white" }}
-          onClick={() => navigate("/sign_up")}
-        >
-          Sign Up
-        </button>
-        <button
-          style={{ margin: "0 20px", backgroundColor: "white", color: "blue" }}
-          onClick={() => navigate("/login")}
-        >
-          Login
-        </button>
+        {user ? (
+          <button
+            style={{
+              margin: "0 20px",
+              backgroundColor: "white",
+              color: "blue",
+            }}
+            onClick={handleLogOut}
+          >
+            Log out
+          </button>
+        ) : (
+          <>
+            <button
+              style={{
+                margin: "0 20px",
+                backgroundColor: "blue",
+                color: "white",
+              }}
+              onClick={() => navigate("/sign_up")}
+            >
+              Sign Up
+            </button>
+            <button
+              style={{
+                margin: "0 20px",
+                backgroundColor: "white",
+                color: "blue",
+              }}
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
